3151c188f62e9b1cfb14365ace177cbab5257af7
lyrihkaesa <kaesalyrih@gmail.com>
2023-04-17 09:33:13 +0700

    fix: `Login` & `Register` can navigation

--- app/src/main/java/com/udinus/aplikasimobile/activity/Login.java
+++ app/src/main/java/com/udinus/aplikasimobile/activity/Login.java
@@ -2,15 +2,25 @@ package com.udinus.aplikasimobile.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.udinus.aplikasimobile.MainActivity;
 import com.udinus.aplikasimobile.R;
+import com.udinus.aplikasimobile.databinding.ActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLogin.setOnClickListener(v -> startActivity(new Intent(Login.this, MainActivity.class)));
+        binding.btnSwitchRegister.setOnClickListener(v -> startActivity(new Intent(Login.this, Register.class)));
     }
 }
--- app/src/main/java/com/udinus/aplikasimobile/activity/Register.java
+++ app/src/main/java/com/udinus/aplikasimobile/activity/Register.java
@@ -2,15 +2,23 @@ package com.udinus.aplikasimobile.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.udinus.aplikasimobile.R;
+import com.udinus.aplikasimobile.databinding.ActivityRegisterBinding;
 
 public class Register extends AppCompatActivity {
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.btnRegister.setOnClickListener(v -> startActivity(new Intent(Register.this, Login.class)));
+        binding.btnChangeLogin.setOnClickListener(v -> startActivity(new Intent(Register.this, Login.class)));
     }
 }
